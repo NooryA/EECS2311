6e2dd53b9eb8798f457df52c39ff6144ac4ce778
marlinla <marlinl@my.yorku.ca>
2020-01-22 13:01:43 -0500

    Creation of develop branch
    Clean slate of JavaFX Scene

--- Venn/src/main/java/Venn/Main.java
+++ Venn/src/main/java/Venn/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		System.out.println("push #2... ");
 		System.out.println("push #3");
 		
-		
+	    System.out.println("Feature #1");
 		System.out.println("merge attempt");
 
 	}
